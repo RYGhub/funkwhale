eef2ff3f9a1851973e920d3ba71e80d7d589dd33
Eliot Berriot <contact@eliotberriot.com>
2018-03-04 16:05:41 +0100

    Fixed broken daphne and removed useless templates

--- api/compose/django/daphne.sh
+++ api/compose/django/daphne.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -eux
 python /app/manage.py collectstatic --noinput
-/usr/local/bin/daphne --root-path=/app -b 0.0.0.0 -p 5000 config.asgi:application
+/usr/local/bin/daphne -b 0.0.0.0 -p 5000 config.asgi:application
