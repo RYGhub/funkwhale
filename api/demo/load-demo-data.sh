d1d06ce760662f6c3221dab53a2a2cc7ee2b3fbb
Eliot Berriot <contact@eliotberriot.com>
2018-03-04 18:15:30 +0100

    Bash script to setup / reload the demo, fix #29

--- api/demo/load-demo-data.sh
+++ api/demo/load-demo-data.sh
@@ -6,8 +6,8 @@ python manage.py migrate --noinput
 
 echo "Creating demo user..."
 
-cat demo/demo-user.py | python manage.py shell --plain
+cat demo/demo-user.py | python manage.py shell -i python
 
 echo "Importing demo tracks..."
 
-python manage.py import_files "/music/**/*.ogg" --recursive --noinput
+python manage.py import_files "/music/**/*.ogg" --recursive --noinput --username demo
