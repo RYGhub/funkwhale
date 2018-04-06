4ce9f9bf08a59397645943df9fd8744b2c1ae5ce
Eliot Berriot <contact@eliotberriot.com>
2018-04-06 17:58:16 +0200

    Dedicated permission to access library data via activity pub

---
+++ api/funkwhale_api/federation/permissions.py
@@ -0,0 +1,19 @@
+from django.conf import settings
+
+from rest_framework.permissions import BasePermission
+
+from . import actors
+
+
+class LibraryFollower(BasePermission):
+
+    def has_permission(self, request, view):
+        if not settings.FEDERATION_MUSIC_NEEDS_APPROVAL:
+            return True
+
+        actor = getattr(request, 'actor', None)
+        if actor is None:
+            return False
+
+        library = actors.SYSTEM_ACTORS['library'].get_actor_instance()
+        return library.followers.filter(url=actor.url).exists()
