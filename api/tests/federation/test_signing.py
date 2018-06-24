9017acdb393f955b50f8f6204426ebb0ad67bed2
Eliot Berriot <contact@eliotberriot.com>
2018-06-24 19:17:56 +0200

    Fix #328: Validate Date header in HTTP Signatures

--- api/tests/federation/test_signing.py
+++ api/tests/federation/test_signing.py
@@ -1,4 +1,7 @@
 import cryptography.exceptions
+import datetime
+from django.utils.http import http_date
+from django import forms
 import pytest
 
 from funkwhale_api.federation import keys, signing
@@ -34,6 +37,20 @@ def test_verify_fails_with_wrong_key(nodb_factories):
 
     with pytest.raises(cryptography.exceptions.InvalidSignature):
         signing.verify(prepared_request, wrong_public)
+
+
+def test_verify_fails_with_wrong_date(nodb_factories, now):
+    too_old = now - datetime.timedelta(seconds=31)
+    too_old = http_date(too_old.timestamp())
+    private, public = nodb_factories["federation.KeyPair"]()
+    auth = nodb_factories["federation.SignatureAuth"](key=private)
+    request = nodb_factories["federation.SignedRequest"](
+        auth=auth, headers={"Date": too_old}
+    )
+    prepared_request = request.prepare()
+
+    with pytest.raises(forms.ValidationError):
+        signing.verify(prepared_request, public)
 
 
 def test_can_verify_django_request(factories, fake_request):
@@ -95,14 +112,18 @@ def test_can_verify_django_request_digest_failure(factories, fake_request):
         signing.verify_django(django_request, public_key)
 
 
-def test_can_verify_django_request_failure(factories, fake_request):
+def test_can_verify_django_request_failure(factories, fake_request, now):
     private_key, public_key = keys.get_key_pair()
     signed_request = factories["federation.SignedRequest"](
         auth__key=private_key, auth__headers=["date"]
     )
     prepared = signed_request.prepare()
     django_request = fake_request.get(
-        "/", **{"HTTP_DATE": "Wrong", "HTTP_SIGNATURE": prepared.headers["signature"]}
+        "/",
+        **{
+            "HTTP_DATE": http_date((now + datetime.timedelta(seconds=31)).timestamp()),
+            "HTTP_SIGNATURE": prepared.headers["signature"],
+        }
     )
-    with pytest.raises(cryptography.exceptions.InvalidSignature):
+    with pytest.raises(forms.ValidationError):
         signing.verify_django(django_request, public_key)
