3895034089bc9e4aa8eda2b78c9bd2e7de517bed
Eliot Berriot <contact@eliotberriot.com>
2018-10-01 21:56:52 +0200

    Ensure we always use raw_ids for related fields in django's admin

--- api/config/urls.py
+++ api/config/urls.py
@@ -5,7 +5,7 @@ from django.conf import settings
 from django.conf.urls import url
 from django.urls import include, path
 from django.conf.urls.static import static
-from django.contrib import admin
+from funkwhale_api.common import admin
 from django.views import defaults as default_views
 
 
