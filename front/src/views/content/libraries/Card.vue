e1628a33d2581399382501e879ffea221ecd8a7c
Eliot Berriot <contact@eliotberriot.com>
2018-10-01 18:59:31 +0200

    See #542: fixed pluralization issue

--- front/src/views/content/libraries/Card.vue
+++ front/src/views/content/libraries/Card.vue
@@ -36,8 +36,8 @@
           <i class="database icon"></i>
           {{ library.size | humanSize }}
         </span>
-        <i class="music icon"></i>
-        <translate :translate-params="{count: library.uploads_count}" :translate-n="library.uploads_count" translate-plural="%{ count } tracks">1 tracks</translate>
+        <i class="music icon"></i> {{ library.uploads_count }}
+        <translate :translate-params="{count: library.uploads_count}" :translate-n="library.uploads_count" translate-plural="%{ count } tracks">1 track</translate>
       </div>
     </div>
     <div class="ui bottom basic attached buttons">
