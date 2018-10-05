0a22504fb0de9437bbd9bbcf00c335db85cde80b
Eliot Berriot <contact@eliotberriot.com>
2018-10-05 19:30:46 +0200

    See #559: added a mixin to store common translations

---
+++ front/src/components/mixins/Translations.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+  computed: {
+    sharedLabels () {
+      return {
+        filters: {
+          creation_date: this.$gettext('Creation date'),
+          accessed_date: this.$gettext('Accessed date'),
+          modification_date: this.$gettext('Modification date'),
+          imported_date: this.$gettext('Imported date'),
+          expiration_date: this.$gettext('Expiration date'),
+          track_title: this.$gettext('Track name'),
+          album_title: this.$gettext('Album name'),
+          artist_name: this.$gettext('Artist name'),
+          name: this.$gettext('Name'),
+          title: this.$gettext('Title'),
+          size: this.$gettext('Size'),
+          bitrate: this.$gettext('Bitrate'),
+          duration: this.$gettext('Duration'),
+          date_joined: this.$gettext('Sign-up date'),
+          last_activity: this.$gettext('Last activity'),
+          username: this.$gettext('Username'),
+        }
+      }
+    }
+  }
+}
+</script>
