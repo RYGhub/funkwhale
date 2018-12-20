7ed3a24723a005cb084a1087c5835c81fb57dff2
Eliot Berriot <contact@eliotberriot.com>
2018-12-20 15:03:42 +0100

    Smaller CSS with purge CSS (130kb removed)

--- front/vue.config.js
+++ front/vue.config.js
@@ -1,10 +1,22 @@
 
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const webpack = require('webpack');
+const PurgecssPlugin = require('purgecss-webpack-plugin')
+const glob = require('glob-all')
+const path = require('path')
 
 let plugins = [
   // do not include moment.js locales since it's quite heavy
   new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+  // Remove unused CSS using purgecss. See https://github.com/FullHuman/purgecss
+  // for more information about purgecss.
+  new PurgecssPlugin({
+    paths: glob.sync([
+      path.join(__dirname, './public/index.html'),
+      path.join(__dirname, './**/*.vue'),
+      path.join(__dirname, './src/**/*.js')
+    ])
+  }),
 ]
 if (process.env.BUNDLE_ANALYZE === '1') {
   plugins.push(new BundleAnalyzerPlugin())
