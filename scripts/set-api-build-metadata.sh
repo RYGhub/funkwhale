43e096b1e0f007383941aa0a541add59a53fa289
Eliot Berriot <contact@eliotberriot.com>
2019-01-10 14:55:28 +0100

    Now include commit hash in develop builds

---
+++ scripts/set-api-build-metadata.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -eu
+# given a commit hash, will append this to the version number stored
+# in api/funkwhale_api/__init__.py
+
+commit=$1
+suffix="+git.$commit"
+replace="__version__ = \"\1${suffix}\""
+file="api/funkwhale_api/__init__.py"
+sed -i -E 's@__version__ = \"(.*)\"@'"$replace"'@' $file
