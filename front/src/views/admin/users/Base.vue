e0f11cc70eac985c02ab723159ea79aa657c989a
Jo Vuit <jo.vuitton@gmail.com>
2019-02-09 16:08:19 +0100

    Added translation context

--- front/src/views/admin/users/Base.vue
+++ front/src/views/admin/users/Base.vue
@@ -3,10 +3,10 @@
     <nav class="ui secondary pointing menu" role="navigation" :aria-label="labels.secondaryMenu">
       <router-link
         class="ui item"
-        :to="{name: 'manage.users.users.list'}"><translate>Users</translate></router-link>
+        :to="{name: 'manage.users.users.list'}"><translate :translate-context="'*/Admin/Link'">Users</translate></router-link>
       <router-link
         class="ui item"
-        :to="{name: 'manage.users.invitations.list'}"><translate>Invitations</translate></router-link>
+        :to="{name: 'manage.users.invitations.list'}"><translate :translate-context="'Menu/Admin/Link'">Invitations</translate></router-link>
     </nav>
     <router-view :key="$route.fullPath"></router-view>
   </div>
@@ -17,8 +17,8 @@ export default {
   computed: {
     labels() {
       return {
-        manageUsers: this.$gettext("Manage users"),
-        secondaryMenu: this.$gettext("Secondary menu")
+        manageUsers: this.$pgettext('Head/Admin/Title', 'Manage users'),
+        secondaryMenu: this.$gettext('Menu/*/Hidden text','Secondary menu')
       }
     }
   }
