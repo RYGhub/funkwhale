29e8b23a16f6394685b5f75bd410e288c95416e8
jovuit <jo.vuitton@gmail.com>
2019-02-13 08:14:14 +0100

    #662: Resolve "Add contexts to translatable strings"

--- front/src/views/content/Base.vue
+++ front/src/views/content/Base.vue
@@ -3,10 +3,10 @@
     <nav class="ui secondary pointing menu" role="navigation" :aria-label="labels.secondaryMenu">
       <router-link
         class="ui item"
-        :to="{name: 'content.libraries.index'}"><translate>Libraries</translate></router-link>
+        :to="{name: 'content.libraries.index'}"><translate :translate-context="'Menu/Library/Tab.Link'">Libraries</translate></router-link>
       <router-link
         class="ui item"
-        :to="{name: 'content.libraries.files'}"><translate>Tracks</translate></router-link>
+        :to="{name: 'content.libraries.files'}"><translate :translate-context="'Menu/Library/Tab.Link'">Tracks</translate></router-link>
     </nav>
     <router-view :key="$route.fullPath"></router-view>
   </main>
@@ -15,7 +15,7 @@
 export default {
   computed: {
     labels() {
-      let title = this.$gettext("Add content")
+      let title = this.$pgettext('Head/Library/Title', "Add content")
       let secondaryMenu = this.$pgettext('Menu/*/Hidden text', "Secondary menu")
       return {
         title,
