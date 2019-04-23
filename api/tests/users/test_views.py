b23e87e102f52cd5db62e2ba503c19274c40a737
Eliot Berriot <contact@eliotberriot.com>
2019-04-23 11:35:59 +0200

    Fix #798: Allow users with dots in their usernames to request a subsonic password

--- api/tests/users/test_views.py
+++ api/tests/users/test_views.py
@@ -225,6 +225,21 @@ def test_user_can_get_subsonic_token(logged_in_api_client):
     assert response.data == {"subsonic_api_token": "test"}
 
 
+def test_user_can_request_new_subsonic_token_uncommon_username(logged_in_api_client):
+    user = logged_in_api_client.user
+    user.username = "firstname.lastname"
+    user.subsonic_api_token = "test"
+    user.save()
+
+    url = reverse(
+        "api:v1:users:users-subsonic-token", kwargs={"username": user.username}
+    )
+
+    response = logged_in_api_client.post(url)
+
+    assert response.status_code == 200
+
+
 def test_user_can_delete_subsonic_token(logged_in_api_client):
     user = logged_in_api_client.user
     user.subsonic_api_token = "test"
