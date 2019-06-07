024addfb4b73c9b492614e33eaeff832387051e8
Eliot Berriot <contact@eliotberriot.com>
2019-06-07 16:41:50 +0200

    Added username and domain filter on /history/listenings endpoint

--- api/funkwhale_api/history/filters.py
+++ api/funkwhale_api/history/filters.py
@@ -1,9 +1,14 @@
+import django_filters
+
 from funkwhale_api.moderation import filters as moderation_filters
 
 from . import models
 
 
 class ListeningFilter(moderation_filters.HiddenContentFilterSet):
+    username = django_filters.CharFilter("user__username")
+    domain = django_filters.CharFilter("user__actor__domain_id")
+
     class Meta:
         model = models.Listening
         hidden_content_fields_mapping = moderation_filters.USER_FILTER_CONFIG[
