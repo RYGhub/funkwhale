2523108b6a90549bce96161d230a82a0567d2dcd
Eliot Berriot <contact@eliotberriot.com>
2019-06-10 14:33:46 +0200

    Fix #847: Use ASCII filename before upload to S3 to avoid playback issues

--- api/config/settings/common.py
+++ api/config/settings/common.py
@@ -323,7 +323,7 @@ if AWS_ACCESS_KEY_ID:
     AWS_S3_REGION_NAME = env("AWS_S3_REGION_NAME", default=None)
     AWS_S3_SIGNATURE_VERSION = "s3v4"
     AWS_LOCATION = env("AWS_LOCATION", default="")
-    DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+    DEFAULT_FILE_STORAGE = "funkwhale_api.common.storage.ASCIIS3Boto3Storage"
 
 # See: https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#std:setting-STATICFILES_DIRS
 STATICFILES_DIRS = (str(APPS_DIR.path("static")),)
