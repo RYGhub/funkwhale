c8135d38b1cf2cbc08b8404514d14cf98d5ec2c1
Eliot Berriot <contact@eliotberriot.com>
2019-07-15 10:52:57 +0200

    See #432: added model tagging logic on artists and albums

--- api/funkwhale_api/tags/factories.py
+++ api/funkwhale_api/tags/factories.py
@@ -2,6 +2,8 @@ import factory
 
 from funkwhale_api.factories import registry, NoUpdateOnCreate
 
+from . import models
+
 
 @registry.register
 class TagFactory(NoUpdateOnCreate, factory.django.DjangoModelFactory):
@@ -18,3 +20,14 @@ class TaggedItemFactory(NoUpdateOnCreate, factory.django.DjangoModelFactory):
 
     class Meta:
         model = "tags.TaggedItem"
+
+
+class TaggableFactory(factory.django.DjangoModelFactory):
+    @factory.post_generation
+    def set_tags(self, create, extracted, **kwargs):
+        if not create:
+            # Simple build, do nothing.
+            return
+
+        if extracted:
+            models.set_tags(self, *extracted)
