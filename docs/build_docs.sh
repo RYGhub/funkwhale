33005ed40a23b41c08c7c87737a8690b91d68583
Eliot Berriot <contact@eliotberriot.com>
2019-08-23 09:47:20 +0200

    Updated latest to latest.txt to avoid future namespace clash

--- docs/build_docs.sh
+++ docs/build_docs.sh
@@ -3,4 +3,4 @@
 python -m sphinx . $BUILD_PATH
 TARGET_PATH="$BUILD_PATH/swagger" ./build_swagger.sh
 python ./get-releases-json.py > $BUILD_PATH/releases.json
-python ./get-releases-json.py --latest > $BUILD_PATH/latest
+python ./get-releases-json.py --latest > $BUILD_PATH/latest.txt
